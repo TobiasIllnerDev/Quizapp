faa5f4c34a62201ecd6e2df18698dd2387389667
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-05-15 14:05:05 +0200

    Added new javascript for all questions

---
+++ scripts/questions.js
@@ -0,0 +1,162 @@
+let questions = [
+    {
+        "question": "Wie heißt der Hauptcharakter aus The Legend of Zelda?",
+        "answer_1": "Zelda",
+        "answer_2": "Link",
+        "answer_3": "Ganon",
+        "answer_4": "Epona",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Spiel gehört nicht zu Riot Games?",
+        "answer_1": "Valorant",
+        "answer_2": "League of Legends",
+        "answer_3": "Teamfight Tactics",
+        "answer_4": "Overwatch",
+        "right_answer": 4
+    },
+    {
+        "question": "Welche Farbe hat normalerweise Super Marios Mütze?",
+        "answer_1": "Blau",
+        "answer_2": "Rot",
+        "answer_3": "Grün",
+        "answer_4": "Gelb",
+        "right_answer": 2
+    },
+    {
+        "question": "In welchem Spiel gibt es Creeper?",
+        "answer_1": "Terraria",
+        "answer_2": "Roblox",
+        "answer_3": "Minecraft",
+        "answer_4": "Fortnite",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer ist das Maskottchen von Sega?",
+        "answer_1": "Mario",
+        "answer_2": "Sonic",
+        "answer_3": "Crash Bandicoot",
+        "answer_4": "Pikachu",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie heißt die Welt in World of Warcraft?",
+        "answer_1": "Azeroth",
+        "answer_2": "Tamriel",
+        "answer_3": "Midgard",
+        "answer_4": "Pandora",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche Firma entwickelte die PlayStation?",
+        "answer_1": "Nintendo",
+        "answer_2": "Sony",
+        "answer_3": "Microsoft",
+        "answer_4": "Sega",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Pokémon ist Nummer 25?",
+        "answer_1": "Glumanda",
+        "answer_2": "Pikachu",
+        "answer_3": "Bisasam",
+        "answer_4": "Mew",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie viele Spieler sind in einem klassischen Valorant-Team?",
+        "answer_1": "4",
+        "answer_2": "5",
+        "answer_3": "6",
+        "answer_4": "7",
+        "right_answer": 2
+    },
+    {
+        "question": "Welche Ressource braucht man fuer eine Diamant-Spitzhacke in Minecraft?",
+        "answer_1": "Gold",
+        "answer_2": "Eisen",
+        "answer_3": "Diamanten",
+        "answer_4": "Smaragde",
+        "right_answer": 3
+    },
+    {
+        "question": "In welchem Spiel kommt Master Chief vor?",
+        "answer_1": "Halo",
+        "answer_2": "Destiny",
+        "answer_3": "Doom",
+        "answer_4": "Mass Effect",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche Firma entwickelte League of Legends?",
+        "answer_1": "Blizzard",
+        "answer_2": "Riot Games",
+        "answer_3": "Valve",
+        "answer_4": "Epic Games",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie heisst das Koenigreich in Ni no Kuni II?",
+        "answer_1": "Ding Dong Dell",
+        "answer_2": "Evermore",
+        "answer_3": "Barliona",
+        "answer_4": "Midgar",
+        "right_answer": 2
+    },
+    {
+        "question": "Wer ist der beruehmte Gegner von Mario?",
+        "answer_1": "Dr. Eggman",
+        "answer_2": "Bowser",
+        "answer_3": "Ganondorf",
+        "answer_4": "Ridley",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Spiel ist ein Battle Royale?",
+        "answer_1": "Stardew Valley",
+        "answer_2": "Fortnite",
+        "answer_3": "Terraria",
+        "answer_4": "Sims 4",
+        "right_answer": 2
+    },
+    {
+        "question": "Welche Farbe hat normalerweise Sonics Fell?",
+        "answer_1": "Rot",
+        "answer_2": "Blau",
+        "answer_3": "Gruen",
+        "answer_4": "Schwarz",
+        "right_answer": 2
+    },
+    {
+        "question": "In welchem Spiel gibt es den Nether?",
+        "answer_1": "Terraria",
+        "answer_2": "Roblox",
+        "answer_3": "Minecraft",
+        "answer_4": "Valheim",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer entwickelte Detroit: Become Human?",
+        "answer_1": "Rockstar Games",
+        "answer_2": "Naughty Dog",
+        "answer_3": "Quantic Dream",
+        "answer_4": "Ubisoft",
+        "right_answer": 3
+    },
+    {
+        "question": "Welche Plattform gehoert zu Nintendo?",
+        "answer_1": "Xbox",
+        "answer_2": "PlayStation",
+        "answer_3": "Switch",
+        "answer_4": "Steam Deck",
+        "right_answer": 3
+    },
+    {
+        "question": "Was sammelt Pac-Man hauptsaechlich?",
+        "answer_1": "Muenzen",
+        "answer_2": "Sterne",
+        "answer_3": "Punkte/Pellets",
+        "answer_4": "Schluessel",
+        "right_answer": 3
+    }
+];
